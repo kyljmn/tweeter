0cb327591939871d9df0a9ed73169973f98a9550
kyljmn <kylejemino@gmail.com>
2020-10-27 13:13:40 +0800

    Adds rspec test for users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
+require 'faker'
+
 FactoryBot.define do
   factory :user do
-    
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    username { Faker::Internet.username }
+    email { Faker::Internet.email }
+    password { 'rspecpassword' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when validating a user" do
+    let!(:user1) { FactoryBot.create :user }
+    let(:taken_username) { FactoryBot.build :user, username: user1.username }
+    let(:taken_email) { FactoryBot.build :user, email: user1.email }
+    let(:blank_first_name) { FactoryBot.build :user, first_name: "" }
+    let(:short_first_name) { FactoryBot.build :user, first_name: "a" }
+    let(:blank_last_name) { FactoryBot.build :user, last_name: "" }
+    let(:short_last_name) { FactoryBot.build :user, last_name: "a" }
+    let(:blank_password) { FactoryBot.build :user, password: "" }
+    
+    it "is valid with valid attributes" do
+      expect(user1).to be_valid
+    end
+
+    it "isn't valid when username is already taken" do
+      expect(taken_username).to_not be_valid
+    end
+
+    it "isn't valid when email is already taken" do
+      expect(taken_email).to_not be_valid
+    end
+
+    it "isn't valid when first_name is blank" do
+      expect(blank_first_name).to_not be_valid
+    end
+
+    it "isn't valid when first_name is less than 2 characters" do
+      expect(short_first_name).to_not be_valid
+    end
+
+    it "isn't valid when last_name is blank" do
+      expect(blank_last_name).to_not be_valid
+    end
+
+    it "isn't valid when last_name is less than 2 characters" do
+      expect(short_last_name).to_not be_valid
+    end
+
+    it "isn't valid when password is blank" do
+      expect(blank_password).to_not be_valid
+    end
+  end
 end

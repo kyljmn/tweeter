dd03ecfc81b3762c5405f791ef9fcd77d24fd296
kyljmn <kylejemino@gmail.com>
2020-10-27 12:11:02 +0800

    Adds nav bar, home controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, length: { minimum: 2 }, uniqueness: true
+
+  validates :first_name, presence: true, length: { minimum: 2}
+
+  validates :last_name, presence: true, length: { minimum: 2}
+
+  validates :email, presence: true, uniqueness: true
+  
+  validates :password, presence: true
+  
 end

b20465c31fec7d9c845855cee91bae968114d662
kyljmn <kylejemino@gmail.com>
2020-11-03 15:29:17 +0800

    Fixes heroku error. attempt 2

--- app/models/retwit.rb
+++ app/models/retwit.rb
@@ -3,7 +3,7 @@ class Retwit < ApplicationRecord
 
   belongs_to :retwiter, foreign_key: :retwiter_id, class_name: "User"
 
-  has_one :post, foreign_key: :postable_id, class_name: "Post", dependent: :destroy
+  has_one :post, foreign_key: :postable_id, class_name: "Post"
   
   validates :twit, uniqueness: { scope: :retwiter }
 

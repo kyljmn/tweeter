4ab955be377bf229ab2591ed69296166782b1394
kyljmn <kylejemino@gmail.com>
2020-11-05 16:34:42 +0800

    Adds update feature to user controller.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,7 @@
 class UserController < ApplicationController
   before_action :authenticate_user!
+  before_action :user_from_params, only: %i[show edit update]
+  before_action :auth_guard, only: %i[edit update]
   
   def index
     @users = User.all
@@ -8,11 +10,35 @@ class UserController < ApplicationController
 
   def show
     @twit = Twit.new(twit_params)
-    @user = User.find(params[:id])
     @pager = PagesService.new('post', @user, params.fetch(:page, 1).to_i)
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render 'user/edit'
+    end
+  end
+
   private
+    def user_from_params
+      @user = User.find(params[:id])
+    end
+
+    def auth_guard
+      if @user.id != current_user.id
+        redirect_to root_path
+      end
+    end
+
+    def user_params
+      params.require(:user).permit(:profile_picture)
+    end
+
     def twit_params
       return nil unless !params[:twit].nil?
       params.fetch(:twit, nil).permit(:body)

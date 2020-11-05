c400cf10941096af4033f7a43b4d5be81c3d172b
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-11-05 16:08:51 +0800

    Edits test for user validation testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context "when validating a user" do
-    let!(:user1) { FactoryBot.create :user }
-    let(:taken_username) { FactoryBot.build :user, username: user1.username }
-    let(:taken_email) { FactoryBot.build :user, email: user1.email }
-    let(:blank_first_name) { FactoryBot.build :user, first_name: "" }
-    let(:short_first_name) { FactoryBot.build :user, first_name: "a" }
-    let(:blank_last_name) { FactoryBot.build :user, last_name: "" }
-    let(:short_last_name) { FactoryBot.build :user, last_name: "a" }
-    let(:blank_password) { FactoryBot.build :user, password: "" }
+    let!(:user1) { create :user }
+    let(:taken_username) { build :user, username: user1.username }
+    let(:taken_email) { build :user, email: user1.email }
+    let(:blank_first_name) { build :user, first_name: "" }
+    let(:short_first_name) { build :user, first_name: "a" }
+    let(:blank_last_name) { build :user, last_name: "" }
+    let(:short_last_name) { build :user, last_name: "a" }
+    let(:blank_password) { build :user, password: "" }
     
     it "is valid with valid attributes" do
       expect(user1).to be_valid

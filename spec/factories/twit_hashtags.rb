69add018c37caa816d6d46109ba0aee792ff3263
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-11-05 16:08:01 +0800

    Adds factory for twit_hashtags

--- spec/factories/twit_hashtags.rb
+++ spec/factories/twit_hashtags.rb
@@ -1,6 +1,11 @@
 FactoryBot.define do
   factory :twit_hashtag do
-    twit { nil }
-    hashtag { nil }
+    twit_id { twit.id }
+    hashtag_id { hashtag.id }
+
+    transient do
+      twit { create(:twit) }
+      hashtag { create(:hashtag) }
+    end
   end
 end

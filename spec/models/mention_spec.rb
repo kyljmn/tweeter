726f7d4999c235b7c39acf8643e53264563acf5a
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-11-05 16:09:34 +0800

    Adds testing for validation of mention model

--- spec/models/mention_spec.rb
+++ spec/models/mention_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Mention, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when validating a Mention " do
+   let!(:mention) { create(:mention) }
+   let(:mention_clone) { build(:mention, user_id: mention.user_id, twit_id: mention.twit_id) }
+  
+   it "is valid if its unique" do
+    expect(mention).to be_valid 
+    end
+
+    it "is invalid if mention already exist " do
+      expect(mention_clone).not_to be_valid  
+    end
+  end
 end

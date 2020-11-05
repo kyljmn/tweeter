a1dbfd0769284f25607e9e0ba108a57d9aa41d46
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-11-05 16:10:51 +0800

    Adds test for validation fo twit_hashtag model

--- spec/models/twit_hashtag_spec.rb
+++ spec/models/twit_hashtag_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe TwitHashtag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when validating a twit_hashtag " do
+   let!(:twit_hashtag) { create(:twit_hashtag) }
+   let(:twit_hashtag_clone) { build(:twit_hashtag, hashtag_id: twit_hashtag.hashtag_id, twit_id: twit_hashtag.id) }
+  
+   it "is valid if its unique" do
+    expect(twit_hashtag).to be_valid 
+    end
+
+    it "is invalid if TwitHashtag already exist" do
+      expect(twit_hashtag_clone).not_to be_valid  
+    end
+  end
 end

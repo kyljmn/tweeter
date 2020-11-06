947adffb5886e209d47e712748b44b51d7f31700
ayabbear <jonnah.paris@gmail.com>
2020-11-06 19:52:22 +0800

    fixed files

--- app/helpers/notification_helper.rb
+++ app/helpers/notification_helper.rb
@@ -8,9 +8,7 @@ module NotificationHelper
   end
 
   def notif_message(notif)
-    if notif.notifiable_type == 'Mention'
-      return "<span class=\"font-bold\">@#{notifying_user(notif).username}</span> mentioned you".html_safe 
-    end
+    return "<span class=\"font-bold\">@#{notifying_user(notif).username}</span> mentioned you".html_safe if notif.notifiable_type == "Mention"
   end
 
   def notif_status(notif)

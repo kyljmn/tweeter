83d98960c7f2c34bbfdf2702f58a129ca8c4cf16
kyljmn <kylejemino@gmail.com>
2020-11-06 09:59:08 +0800

    Fixes devise edit_user_path. Adds link to path in nav bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :profile_picture])
-    devise_parameter_sanitizer.permit(:edit, keys: [:username, :first_name, :last_name, :profile_picture])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name, :profile_picture])
   end
 end

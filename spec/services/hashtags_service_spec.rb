de6a5bd6d85a1de2d9344ac800f851b3a03f01d0
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-11-06 11:10:11 +0800

    Edits contexts to improve readability

--- spec/services/hashtags_service_spec.rb
+++ spec/services/hashtags_service_spec.rb
@@ -1,36 +1,49 @@
 require 'rails_helper'
 
-RSpec.describe HashtagsService, type: :model do
+RSpec.describe HashtagsService do
   describe '#hashtag_creator' do
-    let!(:twit) { create(:twit, body: 'this is a #test') }
+    let(:twit) { create(:twit, body: 'this is a #test') }
     let!(:no_hashtag_twit) { create(:twit, body: 'this is a test') }
     let!(:hashtag) { create(:hashtag, name: "testing") }
     let!(:existing_hashtag) { create(:twit, body: "this is a ##{hashtag.name}") }
-    it "will parse twit and look for a hashtag" do
-      parsed_twit = HashtagsService.hashtag_creator(twit)
-      parsed_twit2 = HashtagsService.hashtag_creator(no_hashtag_twit)
 
-      expect(parsed_twit[0]).to eq("test")
-      expect(parsed_twit2).to eq(nil)
-    end
-
-    context "when it has a hashtag" do
-      it "will create a hashtag if hashtag doesn't exit and include in hashtags of twit" do
-        HashtagsService.hashtag_creator(twit)
-  
-        expect(Hashtag.last.name).to eq("test")  
-        expect(twit.hashtags.last).to eq(Hashtag.last)  
-      end
-  
-      it "will include hashtag in hashtags of twit if it exist" do
-        expect(Hashtag.last.name).to eq(hashtag.name)  
-
-        HashtagsService.hashtag_creator(existing_hashtag)
-        
-        expect(existing_hashtag.hashtags.last).to eq(hashtag)  
+    context "when twit does not have a hashtag" do
+      it "will return nill" do
+        parsed_twit2 = HashtagsService.hashtag_creator(no_hashtag_twit)
+        expect(parsed_twit2).to eq(nil)
       end
     end
-    
+
+    context "when it the twit has a hashtag" do
+      it "will parse twit and look for a hashtag" do
+        parsed_twit = HashtagsService.hashtag_creator(twit)
+        expect(parsed_twit[0]).to eq("test")
+      end
+
+      context "when hashtag is new" do
+        before { HashtagsService.hashtag_creator(twit) } 
+
+        it "will create a new hashtag" do
+          expect(Hashtag.last.name).to eq("test")  
+        end
+
+        it "will add new hashtag in hashtags of twit" do
+          expect(twit.hashtags.last.name).to eq("test")  
+        end
+      end
+
+      context "when hashtag alread exist" do
+        before { HashtagsService.hashtag_creator(existing_hashtag) } 
+
+        it "will verify that hashtag alreay exist" do
+          expect(Hashtag.find(hashtag.id)).to eq(hashtag)
+        end
+
+        it "will add existing hashtag in hashtags of twit" do
+          expect(existing_hashtag.hashtags.last).to eq(hashtag)  
+        end
+      end
+    end 
   
   end
 end
--- spec/services/mentions_service_spec.rb
+++ spec/services/mentions_service_spec.rb
@@ -6,19 +6,27 @@ RSpec.describe MentionsService, type: :model do
     let!(:twit) { create(:twit, body: 'Hi @test') }
     let!(:no_mention_twit) { create(:twit, body: 'Hi test') }
 
-    it "will parse twit to look if there is a mentioned user" do
-      parsed_twit = MentionsService.mention_creator(twit)
-      parsed_twit2 = MentionsService.mention_creator(no_mention_twit)
-
-      expect(parsed_twit[0]).to eq("test")
-      expect(parsed_twit2).to eq(nil)
+    context "when twit does not have a mention" do
+      it 'will return nil' do
+        parsed_twit2 = MentionsService.mention_creator(no_mention_twit)
+        expect(parsed_twit2).to eq(nil)
+      end
     end
     
-    it "will create Mention if there is a mention in twit" do
-      MentionsService.mention_creator(twit)
 
-      expect(Mention.first.twit_id).to  eq(twit.id)
-      expect(Mention.first.user_id).to  eq(mention_user.id)
+    context "when twit has a mention" do
+      let!(:parsed_twit) { MentionsService.mention_creator(twit) }
+
+      it "will fetch the mentioned username of user" do
+        parsed_twit = MentionsService.mention_creator(twit)
+        expect(parsed_twit[0]).to eq("test")
+      end
+
+      it "will create a new mention" do
+        expect(Mention.first.twit_id).to  eq(twit.id)
+        expect(Mention.first.user_id).to  eq(mention_user.id)
+      end
+
     end
   
   end
